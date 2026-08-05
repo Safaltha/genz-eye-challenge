31b537a494ef850228ba4f981b998566d7bf83d7
Gen-z hub <102907743+Safaltha@users.noreply.github.com>
2026-08-05 12:13:35 +0545

    Update hidden-number-questions.js

--- hidden-number-questions.js
+++ hidden-number-questions.js
@@ -1,38 +1,38 @@
 const hiddenNumberQuestions = [
 
 {
-    image:"hidden-images/q1.jpg",
-    question:"Which number is hidden?",
-    options:["3","8","5","9"],
-    answer:0
+    image: "q1.jpg",
+    question: "Which number is hidden?",
+    options: ["3", "8", "5", "9"],
+    answer: 0
 },
 
 {
-    image:"hidden-images/q2.jpg",
-    question:"Which number is hidden?",
-    options:["6","2","8","4"],
-    answer:2
+    image: "q2.jpg",
+    question: "Which number is hidden?",
+    options: ["6", "2", "8", "4"],
+    answer: 2
 },
 
 {
-    image:"hidden-images/q3.jpg",
-    question:"Which number is hidden?",
-    options:["7","1","5","3"],
-    answer:1
+    image: "q3.jpg",
+    question: "Which number is hidden?",
+    options: ["7", "1", "5", "3"],
+    answer: 1
 },
 
 {
-    image:"hidden-images/q4.jpg",
-    question:"Which number is hidden?",
-    options:["9","6","0","8"],
-    answer:3
+    image: "q4.jpg",
+    question: "Which number is hidden?",
+    options: ["9", "6", "0", "8"],
+    answer: 3
 },
 
 {
-    image:"hidden-images/q5.jpg",
-    question:"Which number is hidden?",
-    options:["4","7","2","5"],
-    answer:0
+    image: "q5.jpg",
+    question: "Which number is hidden?",
+    options: ["4", "7", "2", "5"],
+    answer: 0
 }
 
 ];
