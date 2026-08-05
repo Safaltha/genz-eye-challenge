84472e86a07bb8115581fae9082d339d14b0141d
Gen-z hub <102907743+Safaltha@users.noreply.github.com>
2026-08-05 12:24:05 +0545

    Update hidden-number-questions.js

--- hidden-number-questions.js
+++ hidden-number-questions.js
@@ -3,36 +3,36 @@ const hiddenNumberQuestions = [
 {
     image: "q1.jpg",
     question: "Which number is hidden?",
-    options: ["3", "8", "5", "9"],
-    answer: 0
+    options: ["3", "29", "5", "9"],
+    answer: 1
 },
 
 {
     image: "q2.jpg",
     question: "Which number is hidden?",
-    options: ["6", "2", "8", "4"],
+    options: ["6", "2", "45", "4"],
     answer: 2
 },
 
 {
     image: "q3.jpg",
     question: "Which number is hidden?",
-    options: ["7", "1", "5", "3"],
+    options: ["7", "73", "5", "3"],
     answer: 1
 },
 
 {
     image: "q4.jpg",
     question: "Which number is hidden?",
-    options: ["9", "6", "0", "8"],
-    answer: 3
+    options: ["16", "6", "0", "8"],
+    answer: 0
 },
 
 {
     image: "q5.jpg",
     question: "Which number is hidden?",
-    options: ["4", "7", "2", "5"],
-    answer: 0
+    options: ["4", "82", "2", "5"],
+    answer: 1
 }
 
 ];
